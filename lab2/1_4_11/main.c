942103881b9f7064b513c4bf4a426688119e52bf
konrad1503 <fresher002@gmail.com>
2023-03-02 20:16:56 +0100

    1_4_11_corrected

--- lab2/1_4_11/main.c
+++ lab2/1_4_11/main.c
@@ -1,42 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
-int main() {
-    int a, b, c, d;
-    printf("Enter four numbers:\n");
-    scanf("%d %d %d %d", &a, &b, &c, &d);
+int zad_a(int a, int b, int c, int d){
+	if (c > d){
+		return 0;
+	}
 
-    // part a
-    int x = 0;
-    while (abs(a) * x * x + b * x + c <= d) {
-        x++;
-    }
-    printf("A)%d\n", x);
+	for (int i = 1; i < INT_MAX; i++){
+		if ((abs(a) * pow(i, 2) + b * pow(i, 2) + c) > d){
+			return i;
+		}
+	}
+}
 
-    // part b
-    x = 0;
-    while (5 * x * x + a * x + b < c) {
-        x++;
-    }
-    if(x-1<0){
-        printf("B)Such x doesn't exist.\n");
-    }
-    else{
-        printf("B)%d\n", x - 1);
-    }
+int zad_b(int a, int b, int c){
+	int prev = 0;
+	bool wasPrev = false;
+	for (int i = 1; i < INT_MAX; i++){
+		if ((5 * pow(i, 2) + a * i + b) < c){
+			prev = i;
+			wasPrev = true;
+		}
+		else if (wasPrev){
+			return prev;
+		}
+	}
+}
 
-    // part c
-    x = 0;
-    while (5 * x * x + a * x + b <= c) {
-        x++;
-    }
-    if(x-1<0){
-        printf("C)Such x doesn't exist.\n");
-    }
-    else{
-        printf("C)%d\n", x - 1);
-    }
+int zad_c(int a, int b, int c){
+	int prev = 0;
+	bool wasPrev = false;
+	for (int i = 1; i < INT_MAX; i++){
+		if ((5 * pow(i, 2) + a * i + b) <= c){
+			prev = i;
+			wasPrev = true;
+		}
+		else if (wasPrev){
+			return prev;
+		}
+	}
+}
 
-    return 0;
+int main(){
+	int a, b, c, d;
+	while (1){
+        printf("Enter four numbers:\n");
+        scanf("%d %d %d %d", &a, &b, &c, &d);
+        if (a != 0){
+			break;
+        }
+		printf("Podaj liczby jeszcze raz!\n");
+	}
+	printf("A) %d\n", zad_a(a, b, c, d));
+	printf("B) %d\n", zad_b(a, b, c));
+	printf("C) %d\n", zad_c(a, b, c));
 }

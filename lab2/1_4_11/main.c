9faaa5eb88be025f5684f85d985f89730f6abd96
konrad1503 <fresher002@gmail.com>
2023-03-12 20:48:15 +0100

    1_4_11_corrected?

--- lab2/1_4_11/main.c
+++ lab2/1_4_11/main.c
@@ -3,45 +3,6 @@
 #include <math.h>
 #include <stdbool.h>
 
-int zad_a(int a, int b, int c, int d){
-	if (c > d){
-		return 0;
-	}
-
-	for (int i = 1; i < INT_MAX; i++){
-		if ((abs(a) * pow(i, 2) + b * pow(i, 2) + c) > d){
-			return i;
-		}
-	}
-}
-
-int zad_b(int a, int b, int c){
-	int prev = 0;
-	bool wasPrev = false;
-	for (int i = 1; i < INT_MAX; i++){
-		if ((5 * pow(i, 2) + a * i + b) < c){
-			prev = i;
-			wasPrev = true;
-		}
-		else if (wasPrev){
-			return prev;
-		}
-	}
-}
-
-int zad_c(int a, int b, int c){
-	int prev = 0;
-	bool wasPrev = false;
-	for (int i = 1; i < INT_MAX; i++){
-		if ((5 * pow(i, 2) + a * i + b) <= c){
-			prev = i;
-			wasPrev = true;
-		}
-		else if (wasPrev){
-			return prev;
-		}
-	}
-}
 
 int main(){
 	int a, b, c, d;
@@ -51,9 +12,81 @@ int main(){
         if (a != 0){
 			break;
         }
-		printf("Podaj liczby jeszcze raz!\n");
+		printf("Enter numbers again!\n");
 	}
-	printf("A) %d\n", zad_a(a, b, c, d));
-	printf("B) %d\n", zad_b(a, b, c));
-	printf("C) %d\n", zad_c(a, b, c));
+
+    if (a<0){
+        a*=-1;
+    }
+    if (c>d){
+        printf("Part A: 0\n");
+    }
+    else{
+        int i;
+        for(i=0;a*i*i+b*i+c<=d;i++){
+            ;
+        }
+        printf("Part A: %d\n",i);
+    }
+
+    int x=0;
+    int max_x=0;
+    if (b<c)
+    {
+        int result = 5*x*x+a*x+b;
+        while(result <c)
+        {
+            max_x=x;
+            x++;
+            result = 5*x*x+a*x+b;
+        }
+    }
+    else
+    {
+        int result = 5*x*x+a*x+b;
+        while(result >=c)
+        {
+            max_x=x;
+            x++;
+            result = 5*x*x+a*x+b;
+        }
+        while(result <c)
+        {
+            max_x=x;
+            x++;
+            result = 5*x*x+a*x+b;
+        }
+    }
+    printf("Part b: %d\n", max_x);
+
+    int x1=0;
+    int max_x1=0;
+    if (b<c)
+    {
+        int result1 = 5*x1*x1+a*x1+b;
+        while(result1 <=c)
+        {
+            max_x1=x1;
+            x1++;
+            result1 = 5*x1*x1+a*x1+b;
+        }
+    }
+    else
+    {
+        int result1 = 5*x1*x1+a*x1+b;
+        while(result1 >c)
+        {
+            max_x1=x1;
+            x1++;
+            result1 = 5*x1*x1+a*x1+b;
+        }
+        while(result1 <=c)
+        {
+            max_x1=x1;
+            x1++;
+            result1 = 5*x1*x1+a*x1+b;
+        }
+    }
+    printf("Part c: %d\n", max_x1);
+    return 0;
 }
